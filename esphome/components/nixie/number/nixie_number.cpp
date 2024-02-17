96bef9976d634260b0b6aa7d0c846798d9d19006
Lincoln Lavoie <lincoln.lavoie@gmail.com>
2024-02-16 20:13:21 -0500

    Clean up and corrected statics
    * Cleaned up private object naming scheme.
    * Removed statis for count back, etc.

--- esphome/components/nixie/number/nixie_number.cpp
+++ esphome/components/nixie/number/nixie_number.cpp
@@ -9,7 +9,7 @@ static const char *const TAG = "nixie.number";
 void NixieNumberComponent::setup() {
   ESP_LOGCONFIG(TAG, "Setting up nixie number...");
   // initialize pins
-  for (auto *pin : this->_pins) {
+  for (auto pin : this->pins_) {
     pin->setup();
     pin->digital_write(false);
   }
@@ -17,40 +17,40 @@ void NixieNumberComponent::setup() {
 
 void NixieNumberComponent::dump_config() {
   LOG_NUMBER("", "Nixie Number", this);
-  ESP_LOGCONFIG(TAG, "  Count Back: %s", YESNO(this->_count_back));
-  ESP_LOGCONFIG(TAG, "  Count Back Speed: %d", this->_count_back_speed);
+  ESP_LOGCONFIG(TAG, "  Count Back: %s", YESNO(this->count_back_));
+  ESP_LOGCONFIG(TAG, "  Count Back Speed: %d", this->count_back_speed_);
   ESP_LOGCONFIG(TAG, "  Output Pins:");
-  for (auto pin : this->_pins)
+  for (auto pin : this->pins_)
     LOG_PIN("    Pin: ", pin);
   LOG_UPDATE_INTERVAL(this);
 }  // NixieNumberComponent::dump_config()
 
 void NixieNumberComponent::loop() {
-  static unsigned long count_back_last_called = 0;
-  if (this->_my_current_number > this->_my_target_number) {
-    if (millis() > count_back_last_called + this->_count_back_speed) {
-      count_back_last_called = millis();
-      this->_set_outputs(--this->_my_current_number);
+  if (this->count_back_ && this->my_current_number_ > this->my_target_number_) {
+    if (millis() > this->count_back_last_called_ + this->count_back_speed_) {
+      this->count_back_last_called_ = millis();
+      this->set_outputs_(--this->my_current_number_);
     }
   }
 }  // NixieNumberComponent::loop()
 
 void NixieNumberComponent::control(float value) {
-  this->_my_target_number = static_cast<uint8_t>(value);
-  if (!this->_count_back || this->_my_current_number < value) {
-    this->_my_current_number = this->_my_target_number;
+  this->my_target_number_ = static_cast<uint8_t>(value);
+  if (!this->count_back_ || this->my_current_number_ < this->my_target_number_) {
+    this->my_current_number_ = this->my_target_number_;
   }
-  this->_set_outputs(this->_my_current_number);
-  this->publish_state(this->_my_target_number);
+  this->set_outputs_(this->my_current_number_);
+  this->publish_state(this->my_target_number_);
 }  // NixieNumberComponent::control(float value)
 
-void NixieNumberComponent::update() { this->publish_state(this->_my_target_number); }
+void NixieNumberComponent::update() { this->publish_state(this->my_target_number_); }
 
-void NixieNumberComponent::_set_outputs(uint8_t value) {
-  static auto previous_value = 0;
-  this->_pins[previous_value]->digital_write(false);
-  previous_value = value;
-  this->_pins[value]->digital_write(true);
+void NixieNumberComponent::set_outputs_(uint8_t value) {
+  for (auto pin : this->pins_)
+    pin->digital_write(false);
+  // this->pins_[this->previous_value_]->digital_write(false);
+  // this->previous_value_ = value;
+  this->pins_[value]->digital_write(true);
 }  // NixieNumberComponent::_set_outputs(uint8_t value)
 
 }  // namespace nixie_number
--- esphome/components/nixie/number/nixie_number.h
+++ esphome/components/nixie/number/nixie_number.h
@@ -9,32 +9,31 @@
 namespace esphome {
 namespace nixie_number {
 
-class NixiePins : public GPIOPin {};
-
 class NixieNumberComponent : public number::Number, public PollingComponent {
  protected:
-  std::vector<GPIOPin *> _pins;
-  bool _count_back{false};
-  uint8_t _count_back_speed{5};
-  uint8_t _my_current_number{0};
-  uint8_t _my_target_number{0};
-
   void control(float value) override;
 
  public:
   NixieNumberComponent() = default;
   void setup() override;
   void dump_config() override;
-  float get_setup_priority() const { return setup_priority::IO; }
+  float get_setup_priority() const { return setup_priority::LATE; }
   void update() override;
   void loop() override;
 
-  void set_count_back(bool count_back) { this->_count_back = count_back; };
-  void set_count_back_speed(uint8_t speed) { this->_count_back_speed = speed; };
-  void set_pins(std::vector<GPIOPin *> pins) { _pins = std::move(pins); };
+  void set_count_back(bool count_back) { this->count_back_ = count_back; };
+  void set_count_back_speed(uint8_t speed) { this->count_back_speed_ = speed; };
+  void set_pins(std::vector<GPIOPin *> pins) { pins_ = std::move(pins); };
 
  private:
-  void _set_outputs(uint8_t value);
+  uint8_t previous_value_{0};
+  unsigned long count_back_last_called_{0};
+  bool count_back_{false};
+  uint8_t count_back_speed_{5};
+  uint8_t my_current_number_{0};
+  uint8_t my_target_number_{0};
+  std::vector<GPIOPin *> pins_;
+  void set_outputs_(uint8_t value);
 };
 
 }  // namespace nixie_number
